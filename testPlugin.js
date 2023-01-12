805f293c467ac8afbf32976541834807fdc3745d
Josep Bernad <jbernadespinosa@gmail.com>
2023-01-12 23:20:14 +0100

    Stats player in done.

--- testPlugin.js
+++ testPlugin.js
@@ -1,33 +1,21 @@
 const PlayerEvents = {
-  onLoadStart: 'loadstart',
-  onWaiting: 'waiting',
-  onSeeking: 'seeking',
-  onSeeked: 'seeked',
   onEnded: 'ended',
   onPlay: 'play',
   onFirstPlay: 'firstplay',
-  onPause: 'pause',
-  onDurationChange: 'durationchange',
-  onFullscreenChange: 'fullscreenchange'
+  onPause: 'pause'
 }
 
 const PlayerEventsIds = {
-  onLoadStart: 0,
-  onWaiting: 1,
-  onSeeking: 2,
-  onSeeked: 3,
   onEnded: 4,
   onPlay: 5,
   onFirstPlay: 6,
-  onPause: 7,
-  onDurationChange: 8,
-  onFullscreenChange: 9
+  onPause: 7
 }
 
 const PlayerEventsUi = {
   4: 'Ended',
   5: 'Playing',
-  6: 'Playing',
+  6: 'Initial Play',
   7: 'Paused',
 }
 
@@ -59,7 +47,7 @@ function testPlugin(options) {
 class NpawPlugin {
 
   constructor(userId, contentId) {
-    this.ui = new PluginUi();
+    this.ui = new NpawPluginUi();
 
     this.id = crypto.randomUUID();
     this.timestamp = Date.now();
@@ -98,7 +86,14 @@ class NpawPlugin {
   }
 
   newViewingSessionEvent(contentPosition, eventId) {
-    this.sessionEvents.push(new SessionEvent(contentPosition, eventId));
+    if (eventId === PlayerEventsIds.onPlay && this.sessionEvents.length === 1) {
+      // Avoid the redundant play event after the firstPlay
+      return;
+    }
+
+    let sessionEvent = new SessionEvent(contentPosition, eventId);
+
+    this.sessionEvents.push(sessionEvent);
 
     if (1 === 0 && this.sessionEvents !== []) {
       // TODO: Handle authorization
@@ -117,35 +112,110 @@ class NpawPlugin {
       });
     }
 
-    this.ui.newEvent(eventId);
+    this.ui.newSessionEvent(sessionEvent);
     console.log('Session data sent');
-    console.log(this.sessionEvents);
   }
 }
 
 class SessionEvent {
   constructor(contentPosition, eventId) {
-    this.contentPosition = contentPosition;
+    this.position = contentPosition;
     this.eventId = eventId;
     this.timestamp = Date.now();
   }
 }
 
-class PluginUi {
+class NpawPluginUi {
 
-  newEvent(eventId) {
-    let uiText = PlayerEventsUi[eventId];
-    this.updateStateText(uiText);
-    this.addEventItem(uiText);
+  constructor() {
+    this.sessionEvents = [];
   }
 
-  updateStateText(uiText) {
-    document.querySelector('#state-placeholder').innerHTML = uiText;
+  newSessionEvent(sessionEvent) {
+    this.sessionEvents.push(sessionEvent);
+
+    this.updateEventUi(sessionEvent);
   }
 
-  addEventItem(uiText) {
-    document.getElementById('events-list-placeholders').innerHTML += '<li class="mt-2 p-5 bg-blue-500 rounded">' + uiText + '</li>';
+  updateEventUi(sessionEvent) {
+    let eventColor = this.getEventColor(sessionEvent.eventId);
+    let eventText = PlayerEventsUi[sessionEvent.eventId];
+    let eventDate = this.getEventDate(sessionEvent.timestamp);
+    let eventPosition = this.getEventPosition(sessionEvent.position);
+
+    // Current state
+    let eventState = '<div class="mt-2 p-5 ' + eventColor + ' font-extrabold text-2xl rounded">' + eventText + '</div>'
+    document.querySelector('#state-placeholder').innerHTML = eventState;
+
+    // Event list
+    let eventItem = '<li class="mt-2 px-5 py-2 ' + eventColor + '/50 rounded flex flex-row place-content-between">\n' +
+      '              <div>\n' +
+      '                <div class="font-bold text-xl">' + eventText + '</div>\n' +
+      '                <div class="text-white/50">' + eventDate + '</div>\n' +
+      '              </div>\n' +
+      '              <div class="my-auto bg-white/30 py-1 px-2 rounded">\n' +
+                       eventPosition +
+      '              </div>\n' +
+      '            </li>';
+    document.querySelector('#events-list-placeholder').innerHTML += eventItem;
   }
 
+  getEventColor(eventId) {
+    let bgColor = 'bg-blue-500';
+
+    switch (eventId) {
+      case PlayerEventsIds.onFirstPlay:
+        bgColor = 'bg-blue-700';
+        break;
+      case PlayerEventsIds.onPlay:
+        bgColor = 'bg-green-700';
+        break;
+      case PlayerEventsIds.onPause:
+        bgColor = 'bg-yellow-700';
+        break;
+      case PlayerEventsIds.onEnded:
+        bgColor = 'bg-red-700';
+        break;
+    }
+
+    return bgColor;
+  }
+
+  getEventDate(timestamp) {
+    let timestampDate = new Date(timestamp);
+    let dateValues = [
+      timestampDate.getHours(),
+      timestampDate.getMinutes(),
+      timestampDate.getSeconds()
+    ]
+
+    let dateValuesFormatted = dateValues.map(value => {
+      return (value < 10) ? '0' + value.toString() : value.toString();
+    })
+
+    return dateValuesFormatted.join(':');
+  }
+
+  getEventPosition(position) {
+    let hours = Math.floor(position / 3600);
+    let minutes = Math.floor((position % 3600) / 60);
+    let seconds = Math.floor(position % 60);
+
+    let positionValues = [
+      hours,
+      minutes,
+      seconds
+    ]
+
+    if (hours === 0) {
+      positionValues.shift();
+    }
+
+    let positionValuesFormatted = positionValues.map(value => {
+      return (value < 10) ? '0' + value.toString() : value.toString();
+    })
+
+    return positionValuesFormatted.join(':');// + ':' + minutes + ':' + seconds + ':' + milliseconds;
+  }
 
 }
